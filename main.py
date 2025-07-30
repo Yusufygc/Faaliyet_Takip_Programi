7113469b6a53deb013102c12da26251a76b1f6bb
Yusufygc <yusufygc118@gmail.com>
2025-07-30 23:59:08 +0300

    Kontrol icin eklenen kodlar kaldirildi

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # main.py
+import os
+import sqlite3
+import customtkinter as ctk 
+import sys
 from database import init_db
 from gui import run_gui
 
 def main():
+    """
+    Ana uygulama akışını başlatır.
+    Veritabanını başlatır ve GUI'yi çalıştırır.
+    """
     init_db()
     run_gui()
 
