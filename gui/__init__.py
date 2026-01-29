3ef08db01fda3a217de7d828c2b99469d6092ac0
Yusufygc <yusufygc118@gmail.com>
2026-01-29 20:45:16 +0300

    PyQt5 güncellemesi

--- gui/__init__.py
+++
@@ -1,8 +0,0 @@
-from .main_window import MainWindow
-
-def run_gui():
-    """
-    Ana GUI uygulamasını başlatır.
-    """
-    app = MainWindow()
-    app.mainloop()
--- hooks/hook-numpy.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import collect_data_files
-
-# numpy'nin MKL kütüphanelerini bulmasını sağlar
-datas = collect_data_files('numpy.core.mkl')
